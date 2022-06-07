80a8d113af0e111fd9ff34cff2bfa886a9de5be1
Marcos Andre <marcosdev.me@gmail.com>
2022-06-07 19:44:42 -0300

    chore: install prettier and config vscode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../../styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
 
 type Props = {
-  title: string;
-};
+  title: string
+}
 
-export default function Home({ title = "React 123" }: Props) {
+export default function Home({ title = 'React 123' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -21,7 +21,7 @@ export default function Home({ title = "React 123" }: Props) {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
+          Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -62,12 +62,12 @@ export default function Home({ title = "React 123" }: Props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  );
+  )
 }

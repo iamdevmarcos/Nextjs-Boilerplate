a3f4817d5c636d5c5277c8a3ef2740ef25bda9b3
Marcos Andre <marcosdev.me@gmail.com>
2022-06-08 18:02:38 -0300

    chore: update nextjs version and set default values

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,12 +1,15 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'Next.js Boilerplate',
+  description = 'TypeScript, React.js, Next.js e Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo src="/img/logo.svg" alt="Imagem de um átomo." />
-    <S.Title>Next.js Boilerplate</S.Title>
-    <S.Description>
-      TypeScript, React.js, Next.js e Styled Components
-    </S.Description>
+
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Um desenvolvedor de frente para uma tela com código."

dd4db8e32c4b96a85ad48097b8115ecf033dcef4
Marcos Andre <marcosdev.me@gmail.com>
2022-06-09 06:09:18 -0300

    refactor: configuring cypress

---
+++ cypress/e2e/example.ts
@@ -0,0 +1,22 @@
+/// <reference path="../support/index.d.ts" />
+
+describe("Cypress TS", () => {
+  it.skip("should go to Google", () => {
+    cy.google()
+  })
+
+  it("should visit homepage of project", () => {
+    cy.visit("/");
+
+    cy.findByAltText(/Imagem de um átomo./i).should("exist")
+    cy.findByRole("heading", { name: /next.js boilerplate/i }).should("exist")
+  })
+
+  it.skip("should visit my personal website", () => {
+    cy.visit("https://marcosdev.me/")
+
+    cy.findByRole("heading", { name: /marcos andre/i }).should("exist")
+    
+    cy.contains("Lembrete").should("exist").click();
+  })
+})

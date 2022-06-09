bb460a35ed64746114d186fe1adae5087ba9e3da
Marcos Andre <marcosdev.me@gmail.com>
2022-06-09 05:53:15 -0300

    feat: config testing library on cypress

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -1,7 +1,15 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Cypress TS', () => {
-  it('should go to Google', () => {
+  it.skip('should go to Google', () => {
     cy.google()
+  })
+
+  it("should visit my personal website", () => {
+    cy.visit("https://marcosdev.me/")
+
+    cy.findByRole("heading", { name: /marcos andre/i }).should("exist")
+
+    cy.contains("Lembrete").should("exist").click();
   })
 })

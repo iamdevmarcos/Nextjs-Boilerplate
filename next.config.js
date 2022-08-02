343a54f7834880e432e9d0f4698ce6ca600decc4
Marcos Andre <marcosdev.me@gmail.com>
2022-08-02 20:20:00 -0300

    chore: update to use SWC instead of Babel

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
-
 const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  swcMinify: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
   pwa: {
     dest: 'public',
     disable: !isProduction

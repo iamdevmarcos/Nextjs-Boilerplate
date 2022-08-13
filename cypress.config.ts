a1b396c7d504786c35806983e91c644f67373b3b
Marcos Andre <marcosdev.me@gmail.com>
2022-08-13 13:07:41 -0300

    chore: apply custom config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   defaultCommandTimeout: 15000,
+  screenshotOnRunFailure: false,
   video: false,
+  videoUploadOnPasses: false,
+
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
+  }
 })

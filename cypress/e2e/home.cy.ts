aac97b4cd58a419cb5ec771c586bdc89b5b98ae2
Rafael Carrenho <rafael.carrenho@hotmail.com>
2022-09-12 08:36:15 -0300

    fix: jest ts error

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,8 +3,8 @@
 describe("Home Page", () => {
   it("should visit homepage of project and find logo and heading", () => {
     cy.visit("/");
-  
-    cy.findByAltText(/Imagem de um átomo./i).should("exist")
-    cy.findByRole("heading", { name: /next.js boilerplate/i }).should("exist")
+
+    cy.findByAltText(/TypeScript, React.js, Next.js e Styled Components/i).should("exist")
+    cy.findByRole("heading", { name: /Next.js Boilerplate/i }).should("exist")
   })
 })

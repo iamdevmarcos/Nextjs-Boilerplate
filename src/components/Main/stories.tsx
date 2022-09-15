0d7fd831f17a5e5f084da16f63890e22adae4ea5
Marcos Andre <marcosdev.me@gmail.com>
2022-09-15 11:05:07 -0300

    chore: configure storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react'
+
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
+} as Meta
+
+export const Default: Story = (args) => <Main {...args} />
+
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'title basic',
+  description: 'description basic'
+}

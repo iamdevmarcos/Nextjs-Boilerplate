111e82282ad6d2d2abab4f76075458d76db9b502
Marcos Andre <marcosdev.me@gmail.com>
2022-09-21 08:53:35 -0300

    fix: adjust storybook core

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,11 +7,9 @@ module.exports = {
     '@storybook/addon-interactions'
   ],
   framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5'
-  },
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
-  }
+  },
+  typescript: { reactDocgen: false }
 }

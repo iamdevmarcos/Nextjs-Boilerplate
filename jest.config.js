3be16c7bfb0cb40b55bf14b5f72d4104d17346d3
Marcos Andre <marcosdev.me@gmail.com>
2022-10-12 17:37:33 -0300

    change package npm to yarn (#129)
    * chore: use yarn instead npm
    
    * chore: add license and author
    
    * perf: ignore stories on tests
    
    * refactor: adjust generator component template
    
    * fix: remove parameters of router context

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   moduleNameMapper: {
     '^styled-components':

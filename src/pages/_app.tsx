b086b63aed744b2af0314d2a1c12a0c69114a805
Marcos Mendes <marcosdev.me@gmail.com>
2023-01-31 10:41:28 -0300

    feat: implements chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,28 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import GlobalStyles from 'styles/global'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <Head>
         <title>Next.js - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components by Marcos Mendes"
         />
       </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
+
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </ChakraProvider>
   )
 }
 

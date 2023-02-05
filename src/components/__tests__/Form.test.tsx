7b2a823c5895b22e54e84e353e56021402b7a4df
Marcos Mendes <marcosdev.me@gmail.com>
2023-02-05 19:12:13 -0300

    test: add some tests

--- src/components/__tests__/Form.test.tsx
+++ src/components/__tests__/Form.test.tsx
@@ -1,10 +1,42 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Form from 'components/Form'
 
 describe('<Form />', () => {
-  it('should render correctly', () => {
+  beforeEach(() => {
     render(<Form />)
+  })
 
-    expect(screen.getByRole('heading', { name: /Form/i })).toBeInTheDocument()
+  it('should render correctly', () => {
+    const heading = screen.getByRole('heading', {
+      name: /example form/i
+    })
+    expect(heading).toBeInTheDocument()
+
+    const firstNameInput = screen.getByRole('textbox', {
+      name: /first name:/i
+    })
+    expect(firstNameInput).toBeInTheDocument()
+
+    const lastNameInput = screen.getByRole('textbox', {
+      name: /last name:/i
+    })
+    expect(lastNameInput).toBeInTheDocument()
+
+    const ageInput = screen.getByRole('textbox', {
+      name: /age:/i
+    })
+    expect(ageInput).toBeInTheDocument()
+
+    const submitBtn = screen.getByRole('button', { name: /submit/i })
+    expect(submitBtn).toBeInTheDocument()
+  })
+
+  it('should change the input value', () => {
+    const firstNameInput = screen.getByRole('textbox', {
+      name: /first name:/i
+    }) as HTMLInputElement
+
+    fireEvent.change(firstNameInput, { target: { value: 'Marcos' } })
+    expect(firstNameInput.value).toBe('Marcos')
   })
 })

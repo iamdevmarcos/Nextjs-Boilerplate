5245396a59047450a29138b00357be861c8671e2
Marcos Mendes <marcosdev.me@gmail.com>
2023-02-07 19:31:26 -0300

    chore: update meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components by Marcos Mendes"
+          content="A simple project starter to work with TypeScript, React, Next.js, Chakra UI, Hook Form and Zod by Marcos Mendes"
         />
       </Head>
 
